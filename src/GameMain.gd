c97aa52e63c4243ccd2357d7b57b796413ad1517
DS-Garnett-Dan-Nu <garnett.dn@gmail.com>
2022-03-08 19:37:52 +0630

    New

--- src/GameMain.gd
+++ src/GameMain.gd
@@ -1,2 +1,17 @@
 extends Node2D
 
+var a = false
+
+onready var Board = $CanvasLayer/Board
+onready var A = $CanvasLayer/VowelContainer/A
+onready var VC = $CanvasLayer/VowelContainer
+
+func _on_A_pressed():
+	Board.text = "A"
+	VC.remove_child(A)
+	a = true
+
+func _physics_process(delta):
+	if a == true:
+		VC.add_child(A)
+		a= false
--- src/Ref.gd
+++ src/Ref.gd
@@ -1,2 +1,18 @@
-extends Node
+extends Node2D
 
+onready var file = 'res://art/Words.txt'
+onready var label = $CL/Label
+
+
+func _ready():    
+	load_file(file)
+
+func load_file(file):
+	var f = File.new()
+	f.open(file, File.READ)
+	var index = 1
+	while not f.eof_reached():
+		var line = f.get_line()
+		line += " "
+		print(line + str(index))
+		index += 1
